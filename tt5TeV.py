e17425bcdbcd8fbb04313b7ad4ac157ed42609fd
Pieter David <pieter.david@cern.ch>
2020-09-22 11:55:05 +0200

    Update decoration for new samples (postprocessed NanoAODv5)

--- tt5TeV.py
+++ tt5TeV.py
@@ -1,59 +1,56 @@
 from bamboo.analysismodules import NanoAODModule, NanoAODHistoModule, NanoAODSkimmerModule
 import os.path
 
-from bamboo.treedecorators import NanoAODDescription, nanoPUWeightVar, ReadJetMETVar
-description_nanov4_5TeV_data = NanoAODDescription.get("v5", year="2018", isMC=False,
-        systVariations=[ ReadJetMETVar("Jet", "MET", jetsNomName="nom", metNomName="nom") ],
-        removeGroups=["ChsMET_", "L1_", "METFixEE2017_"],
-        removeCollections=["nCorrT1METJet"]
-        )
-description_nanov4_5TeV_MC = NanoAODDescription.get("v5", year="2018", isMC=False,
-        systVariations=[ nanoPUWeightVar, ReadJetMETVar("Jet", "MET", jetsExclVars=["raw", "jesHF", "jesHF_2018", "jesHF_2017", "jesHF_2016"], metNomName="nom", metExclVars=["raw", "nom"], bTaggers=["csvv2", "deepcsv", "deepjet", "cmva"], bTagWPs=["L", "M", "T", "shape"]) ],
-        removeGroups=["ChsMET_", "L1_", "METFixEE2017_", "CaloMET_", "PuppiMET_", "TkMET_", "HTXS_"],
-        removeCollections=["nCorrT1METJet", "nFatJet", "nOtherPV", "nPhoton", "nSV", "nSoftActivityJet", "nSubJet", "nTau", "nTrigObj"]
-        )
+import bamboo.treedecorators as btd
+class ReadVariableVarWithSuffix(btd.ReadVariableVarWithSuffix):
+    def getVarName(self, branchName, collgrpname=None):
+        variNm = btd.normVarName(branchName[len(self.prefix):].lstrip(self.sep))
+        return self.prefix, f"{self.systName}{variNm}" if variNm else self.nomName
+class ReadMuScaleCorrection(btd.NanoSystematicVarSpec):
+    def appliesTo(self, name):
+        return name == "Muon"
+    def nomName(self, name):
+        return ""
+    def getVarName(self, name, collgrpname=None):
+        if name.startswith("corrected"):
+            vari, v = name[len("corrected"):].split("_")
+            return v, f"{self.systName}{btd.normVarName(vari)}" if vari else self.nomName(name)
+class ReadElScaleCorrection(btd.NanoSystematicVarSpec):
+    def appliesTo(self, name):
+        return name == "Electron"
+    def getVarName(self, name, collgrpname=None):
+        if name.split("_")[0] in ("pt", "mass") and len(name.split("_")) >= 2:
+            return name.split("_")[0], "_".join(name.split("_")[1:])
+    def nomName(self, name):
+        return "nom"
+_descr_5TeV_removedGroups = ["CaloMET_", "ChsMET_", "RawMET_", "TkMET_"]
+_descr_5TeV_removedGroups_MC = _descr_5TeV_removedGroups + ["Generator_", "HTXS_"]
+_descr_5TeV_removedCollections = ["nFatJet", "nIsoTrack", "nOtherPV", "nSV", "nSoftActivityJet", "nSubJet", "nTau", "nTrigObj"]
+_descr_5TeV_removedCollections_MC = _descr_5TeV_removedCollections + ["nGenJetAK8", "nGenVisTau", "nSubGenJetAK8"]
+description_nanov5_5TeV_data = btd.NanoAODDescription.get("v5", year="2018", isMC=False,
+        removeGroups=_descr_5TeV_removedGroups, removeCollections=_descr_5TeV_removedCollections,
+        systVariations=[
+            ReadMuScaleCorrection("muScale"),
+            ReadElScaleCorrection("elScale"), ## FIXME implement on the fly
+            btd.ReadJetMETVar("Jet", "MET", jetsNomName="nom", metNomName="nom")
+            ])
+description_nanov5_5TeV_MC = btd.NanoAODDescription.get("v5", year="2018", isMC=True,
+        removeGroups=_descr_5TeV_removedGroups_MC, removeCollections=_descr_5TeV_removedCollections_MC,
+        addGroups=["Pileup_"], addCollections=["nLHEPart"],
+        systVariations=[
+            ReadVariableVarWithSuffix("puWeight"),
+            ReadVariableVarWithSuffix("PrefireWeight"),
+            ReadMuScaleCorrection("muScale"),
+            ReadElScaleCorrection("elScale"), ## FIXME implement on the fly
+            btd.ReadJetMETVar("Jet", "MET", jetsExclVars=["raw"], metNomName="nom", metExclVars=["raw", "nom"])
+            ])
 
 class Nano5TeVBase(NanoAODModule):
     """ Base module for custom 5TeV NanoAODv4 samples """
-    def mergeCounters(self, outF, infileNames, sample=None):
-        ## merge all histograms in the input files
-        from bamboo.root import gbl
-        aFile = gbl.TFile.Open(infileNames[0])
-        outF.cd()
-        histos = { ky.GetName(): aFile.Get(ky.GetName()).Clone() for ky in aFile.GetListOfKeys() if ky.GetClassName() == "TH1F" }
-        for iFN in infileNames[1:]:
-            for i in range(5):
-                oFile = gbl.TFile.Open(iFN)
-                if oFile:
-                    break
-                else:
-                    print(f"Could not open {iFN}, sleeping a bit and then retrying...")
-                    time.sleep(10)
-            if not oFile:
-                raise RuntimeError(f"Could not open {iFN} after five attempts")
-            for hName, h in histos.items():
-                hO = oFile.Get(hName)
-                if hO:
-                    h.Add(oFile.Get(hName))
-        outF.cd()
-        for h in histos.values():
-            h.Write()
-    def readCounters(self, resultsFile):
-        dCount = {}
-        for ky in resultsFile.GetListOfKeys():
-            if ky.GetClassName() == "TH1F":
-                obj = resultsFile.Get(ky.GetName())
-                if obj.GetNbinsX() == 1:
-                    dCount[ky.GetName()] = obj.GetBinContent(1)
-                else:
-                    for i in range(obj.GetNbinsX()):
-                        dCount[f"{ky.GetName()}{i:d}"] = obj.GetBinContent(i+1)
-        return dCount
     def prepareTree(self, tree, sample=None, sampleCfg=None):
         ## Decorate the tree
-        from bamboo.treedecorators import NanoAODDescription, nanoPUWeightVar, ReadJetMETVar
         tree,noSel,be,lumiArgs = super(Nano5TeVBase, self).prepareTree(tree, sample=sample, sampleCfg=sampleCfg,
-                description=(description_nanov4_5TeV_MC if self.isMC(sample) else description_nanov4_5TeV_data))
+                description=(description_nanov5_5TeV_MC if self.isMC(sample) else description_nanov5_5TeV_data))
         return tree,noSel,be,lumiArgs
 
 class Nano5TeVHistoModule(Nano5TeVBase, NanoAODHistoModule):
@@ -67,7 +64,7 @@ class HelloWorld(Nano5TeVHistoModule):
 
         isMC = self.isMC(sample)
         if isMC:
-            noSel = noSel.refine("mcWeight", weight=t.genWeight)
+            noSel = noSel.refine("mcWeight", weight=[ t.genWeight ])
         noSel = noSel.refine("trig", cut=op.OR(t.HLT.HIL3DoubleMu0, t.HLT.HIEle20_Ele12_CaloIdL_TrackIdL_IsoVL_DZ))
 
         plots = []
